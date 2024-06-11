12401cd2821d5c3270011e8c2e75f8bd5d53be41
Muhammadyusufdeveloper <155054579+Muhammadyusufdevloper@users.noreply.github.com>
2024-06-11 19:40:24 +0500

    homework

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-// redux toolkit
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom";
+import Loading from './components/loading/Loading';
+
+// Dinamik yuklanadigan App komponenti
+const App = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Suspense fallback={<Loading />}>
+        <App />
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 );
-
